10332adeaf6edc78707e923e615e192914315011
MAzeemSidd <m.azeem94@gmail.com>
2024-03-21 09:24:33 +0500

    Make routes private so that Unauthorized user can not be access those.

--- src/components/Layouts/DefaultLayout.js
+++ src/components/Layouts/DefaultLayout.js
@@ -1,13 +1,10 @@
 import React, { Suspense, useState } from 'react';
 import {
   ContactsOutlined,
-  DesktopOutlined,
-  FileOutlined,
   HomeOutlined,
   LogoutOutlined,
   PieChartOutlined,
   ProductOutlined,
-  TeamOutlined,
   UnorderedListOutlined,
   UserOutlined,
 } from '@ant-design/icons';
@@ -31,187 +28,112 @@ const items = [
   {key: '/responses', label: 'Responses', icon: <ContactsOutlined />},
   {key: '/reports', label: 'Reports', icon: <PieChartOutlined />},
 ];
-// const App = () => {
-//   const [collapsed, setCollapsed] = useState(false);
-//   const {
-//     token: { colorBgContainer, borderRadiusLG },
-//   } = theme.useToken();
 
-//   const navigate = useNavigate();
 
-//   return (
-//     <Layout
-//       style={{
-//         minHeight: '100vh',
-//       }}
-//     >
-//       <Sider trigger={null} collapsible collapsed={collapsed} /*onCollapse={(value) => setCollapsed(value)}*/>
-//         <div className="demo-logo-vertical" />
-//         <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} onClick={item=>{
-//           console.log('NAVBAR', item.key)
-//             /*if(item.keyPath.length === 1) */navigate(`${item.key}`)
-//             // else if(item.keyPath.length === 2) navigate(`${item.keyPath[1]}/${item.keyPath[0]}`, {state: {id: item.keyPath[0]}});
-//         }} />
-//       </Sider>
-//       <Layout>
-//         <Header style={{padding: 0, background: colorBgContainer}}>
-//           <Button type='text' size='large' onClick={()=>setCollapsed(prev=>!prev)}><UnorderedListOutlined style={{fontSize: 20}} /></Button>
-//         </Header>
-//         <Content style={{margin: '0 16px'}}>
-//           {/* <Breadcrumb style={{margin: '16px 0'}}>
-//             <Breadcrumb.Item>User</Breadcrumb.Item>
-//             <Breadcrumb.Item>Bill</Breadcrumb.Item>
-//           </Breadcrumb>
-//           <div style={{padding: 24, minHeight: 360, background: colorBgContainer, borderRadius: borderRadiusLG,}}>
-//             Bill is a cat.
-//           </div> */}
-//           <Suspense fallback={<Spin spinning={true}/>}>
-//             <Routes>
-//               {routes.map((route, idx) => {
-//                   return (
-//                     route.element && (
-//                       <Route
-//                         key={idx}
-//                         path={route.path}
-//                         exact={route.exact}
-//                         name={route.name}
-//                         element={<route.element />}
-//                         />
-//                     )
-//                   )
-//               })}
-//             </Routes>
-//           </Suspense>
-//         </Content>
-//         <Footer style={{textAlign: 'center'}}>
-//           {/* Ant Design ©{new Date().getFullYear()} Created by Ant UED */}
-//           Created by Virtuosoft
-//         </Footer>
-//       </Layout>
-//     </Layout>
-//   );
-// };
-// ... (previous imports)
-
-const App = () => {
+const DefaultLayout = () => {
   const [collapsed, setCollapsed] = useState(false);
   const navigate = useNavigate();
   const {
     token: { colorBgContainer, borderRadiusLG },
   } = theme.useToken();
 
-  // const handleLogout = () => {
-  //   try {
-  //     console.log("Logging out...");
-  //     localStorage.clear();
-  //     navigate("/", { replace: true });
-  //   } catch (error) {
-  //     console.error("Error during logout:", error);
-  //   }
-  // };
   const handleLogout = () => {
     console.log('log out...');
     localStorage.removeItem("jwtToken");
     navigate('/', { replace: true })
-    // window.open('/');
-    // window.close();
   };
   
-  // const menu = (
-  //   <Menu>
-  //     {/* <Menu.Item key="profile">Profile</Menu.Item>
-  //     <Menu.Item key="settings">Settings</Menu.Item> */}
-  //     <Menu.Item key="logout" onClick={handleLogout}>
-  //       Logout
-  //     </Menu.Item>
-  //   </Menu>
-  // );
-  return (
-    <Layout style={{ minHeight: '100vh' }}>
-      <Sider trigger={null} collapsible collapsed={collapsed}>
-        <div className="demo-logo-vertical" />
-        <Menu
-          theme="dark"
-          defaultSelectedKeys={['1']}
-          mode="inline"
-          onClick={(item) => {
-            console.log('NAVBAR', item.key);
-            navigate(`${item.key}`);
-          }}
-        >
-          {items.map((item) => (
-            <Menu.Item key={item.key} icon={item.icon}>
-              {item.label}
-            </Menu.Item>
-          ))}
-        </Menu>
-      </Sider>
-      <Layout>
-        <Header style={{ padding: 10, background: colorBgContainer, display:'flex', justifyContent:"space-between" }}>
-          <Button
-            type="text"
-            size="large"
-            onClick={() => setCollapsed((prev) => !prev)}
-          >
-            <UnorderedListOutlined style={{ fontSize: 20 }} />
-          </Button>
-          <Dropdown /*overlay={menu}*/ placement="bottomRight" arrow trigger={['click']}
-            menu={{
-              items: [
-                {
-                  id: 1,
-                  label: 
-                    (<a target="_blank" onClick={e=>{e.stopPropagation(); handleLogout();}}>
-                      <LogoutOutlined style={{size: 10, marginRight: 5}} />
-                      Logout
-                    </a>),
-                }
-              ]
+  return (<>
+    {
+      localStorage.getItem("jwtToken") ?
+      <Layout style={{ minHeight: '100vh' }}>
+        <Sider trigger={null} collapsible collapsed={collapsed}>
+          <div className="demo-logo-vertical" />
+          <Menu
+            theme="dark"
+            defaultSelectedKeys={['1']}
+            mode="inline"
+            onClick={(item) => {
+              console.log('NAVBAR', item.key);
+              navigate(`${item.key}`);
             }}
           >
-            <Button type="text" size="large">
-              <Avatar
-                style={{
-                  backgroundColor: '#aaa',
-                }}
-                icon={<UserOutlined />}
-                size="small"
-              />
+            {items.map((item) => (
+              <Menu.Item key={item.key} icon={item.icon}>
+                {item.label}
+              </Menu.Item>
+            ))}
+          </Menu>
+        </Sider>
+        <Layout>
+          <Header style={{ padding: 10, background: colorBgContainer, display:'flex', justifyContent:"space-between" }}>
+            <Button
+              type="text"
+              size="large"
+              onClick={() => setCollapsed((prev) => !prev)}
+            >
+              <UnorderedListOutlined style={{ fontSize: 20 }} />
             </Button>
-          </Dropdown>
-        </Header>
-        <Content style={{margin: '0 16px'}}>
-          {/* <Breadcrumb style={{margin: '16px 0'}}>
-            <Breadcrumb.Item>User</Breadcrumb.Item>
-            <Breadcrumb.Item>Bill</Breadcrumb.Item>
-          </Breadcrumb>
-          <div style={{padding: 24, minHeight: 360, background: colorBgContainer, borderRadius: borderRadiusLG,}}>
-            Bill is a cat.
-          </div> */}
-          <Suspense fallback={<Spin spinning={true}/>}>
-            <Routes>
-              {routes.map((route, idx) => (
-                route.element && (
-                  <Route
-                    key={idx}
-                    path={route.path}
-                    exact={route.exact}
-                    name={route.name}
-                    element={<route.element />}
-                  />
-                )
-              ))}
-            </Routes>
-          </Suspense>
-        </Content>
-        <Footer style={{ textAlign: 'center' }}>
-          Created by Virtuosoft
-        </Footer>
+            <Dropdown /*overlay={menu}*/ placement="bottomRight" arrow trigger={['click']}
+              menu={{
+                items: [
+                  {
+                    id: 1,
+                    label: 
+                      (<a target="_blank" onClick={e=>{e.stopPropagation(); handleLogout();}}>
+                        <LogoutOutlined style={{size: 10, marginRight: 5}} />
+                        Logout
+                      </a>),
+                  }
+                ]
+              }}
+            >
+              <Button type="text" size="large">
+                <Avatar
+                  style={{
+                    backgroundColor: '#aaa',
+                  }}
+                  icon={<UserOutlined />}
+                  size="small"
+                />
+              </Button>
+            </Dropdown>
+          </Header>
+          <Content style={{margin: '0 16px'}}>
+            {/* <Breadcrumb style={{margin: '16px 0'}}>
+              <Breadcrumb.Item>User</Breadcrumb.Item>
+              <Breadcrumb.Item>Bill</Breadcrumb.Item>
+            </Breadcrumb>
+            <div style={{padding: 24, minHeight: 360, background: colorBgContainer, borderRadius: borderRadiusLG,}}>
+              Bill is a cat.
+            </div> */}
+            <Suspense fallback={<Spin spinning={true}/>}>
+              <Routes>
+                {routes.map((route, idx) => (
+                  route.element && (
+                    <Route
+                      key={idx}
+                      path={route.path}
+                      exact={route.exact}
+                      name={route.name}
+                      element={<route.element />}
+                    />
+                  )
+                ))}
+              </Routes>
+            </Suspense>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>
+            Created by Virtuosoft
+          </Footer>
+        </Layout>
       </Layout>
-    </Layout>
-  );
+      :
+      <Navigate to='/' />
+    }
+  </>);
 };
 
-export default App;
+export default DefaultLayout;
 
 // export default App;
