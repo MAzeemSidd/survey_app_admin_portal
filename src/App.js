b352944e049761c74983de28354ff7991f49c0c1
MAzeemSidd <m.azeem94@gmail.com>
2024-03-25 11:16:09 +0500

    Resolve navbar scrolling issue. Also style navbar.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Suspense } from 'react';
 import { BrowserRouter, HashRouter, Route, Routes } from 'react-router-dom';
 import { Spin } from 'antd';
 import Login from './components/Login';
+// import './App.css'
+
 const Projects  = React.lazy(()=>import('./components/projects/Projects'));
 const DefaultLayout = React.lazy(() => import('./components/Layouts/DefaultLayout'));
 const Homepage = React.lazy(() => import('./components/homepage/Homepage'));
--- src/components/Layouts/DefaultLayout.js
+++ src/components/Layouts/DefaultLayout.js
@@ -11,9 +11,10 @@ import {
   UnorderedListOutlined,
   UserOutlined,
 } from '@ant-design/icons';
-import { Avatar, Breadcrumb, Button, Dropdown, Layout, Menu, Spin, theme } from 'antd';
+import { Avatar, Breadcrumb, Button, ConfigProvider, Dropdown, Layout, Menu, Spin, theme } from 'antd';
 import { Navigate, Route, Routes, useNavigate } from 'react-router-dom';
 import routes from '../../routes';
+import "../../App.css";
 
 const { Header, Content, Footer, Sider } = Layout;
 function getItem(label, key, icon, children) {
@@ -50,88 +51,105 @@ const DefaultLayout = () => {
   return (<>
     {
       localStorage.getItem("jwtToken") ?
-      <Layout style={{ minHeight: '100vh' }}>
-        <Sider trigger={null} collapsible collapsed={collapsed}>
-          <div className="demo-logo-vertical" />
-          <Menu
-            theme="dark"
-            defaultSelectedKeys={['1']}
-            mode="inline"
-            onClick={(item) => {
-              console.log('NAVBAR', item.key);
-              navigate(`${item.key}`);
-            }}
-          >
-            {items.map((item) => (
-              <Menu.Item key={item.key} icon={item.icon}>
-                {item.label}
-              </Menu.Item>
-            ))}
-          </Menu>
-        </Sider>
-        <Layout>
-          <Header style={{ padding: 10, background: colorBgContainer, display:'flex', justifyContent:"space-between" }}>
-            <Button
-              type="text"
-              size="large"
-              onClick={() => setCollapsed((prev) => !prev)}
-            >
-              <UnorderedListOutlined style={{ fontSize: 20 }} />
-            </Button>
-            <Dropdown /*overlay={menu}*/ placement="bottomRight" arrow trigger={['click']}
-              menu={{
-                items: [
-                  {
-                    id: 1,
-                    label: 
-                      (<a target="_blank" onClick={e=>{e.stopPropagation(); handleLogout();}}>
-                        <LogoutOutlined style={{size: 10, marginRight: 5}} />
-                        Logout
-                      </a>),
-                  }
-                ]
+      <ConfigProvider
+        theme={{
+          components: {
+            Layout: {
+              /* here is your component tokens */
+              siderBg: '#36454f'
+            },
+            Menu: {
+              darkItemBg: '#36454f',
+              darkItemSelectedBg: '#26343d',
+              darkItemHoverBg: '#375263'
+            }
+          },
+        }}
+      >
+        <Layout hasSider>
+          <Sider trigger={null} collapsible collapsed={collapsed}
+            style={{overflow: 'auto',height: '100vh',position: 'fixed',left: 0,top: 0,bottom: 0}}>
+            <Menu
+              // style={{background: '#36454f'}}
+              theme="dark"
+              defaultSelectedKeys={['1']}
+              mode="inline"
+              onClick={(item) => {
+                console.log('NAVBAR', item.key);
+                navigate(`${item.key}`);
               }}
             >
-              <Button type="text" size="large">
-                <Avatar
-                  style={{
-                    backgroundColor: '#aaa',
-                  }}
-                  icon={<UserOutlined />}
-                  size="small"
-                />
+              {items.map((item) => (
+                <Menu.Item key={item.key} icon={item.icon}>
+                  {item.label}
+                </Menu.Item>
+              ))}
+            </Menu>
+          </Sider>
+          <Layout style={{minHeight: '100vh', marginLeft: collapsed ? 80 : 200, zIndex: 900}}>
+            <Header style={{ padding: 10, background: colorBgContainer, display:'flex', justifyContent:"space-between" }}>
+              <Button
+                type="text"
+                size="large"
+                onClick={() => setCollapsed((prev) => !prev)}
+              >
+                <UnorderedListOutlined style={{ fontSize: 20 }} />
               </Button>
-            </Dropdown>
-          </Header>
-          <Content style={{margin: '0 16px'}}>
-            {/* <Breadcrumb style={{margin: '16px 0'}}>
-              <Breadcrumb.Item>User</Breadcrumb.Item>
-              <Breadcrumb.Item>Bill</Breadcrumb.Item>
-            </Breadcrumb>
-            <div style={{padding: 24, minHeight: 360, background: colorBgContainer, borderRadius: borderRadiusLG,}}>
-              Bill is a cat.
-            </div> */}
-            <Suspense fallback={<Spin spinning={true}/>}>
-              <Routes>
-                {routes.map((route, idx) => (
-                  route.element && (
-                    <Route
-                      key={idx}
-                      path={route.path}
-                      exact={route.exact}
-                      name={route.name}
-                      element={<route.element />}
-                    />
-                  )
-                ))}
-              </Routes>
-            </Suspense>
-          </Content>
-          <Footer style={{ textAlign: 'center' }}>
-            Created by Virtuosoft
-          </Footer>
+              <Dropdown /*overlay={menu}*/ placement="bottomRight" arrow trigger={['click']}
+                menu={{
+                  items: [
+                    {
+                      id: 1,
+                      label: 
+                        (<a target="_blank" onClick={e=>{e.stopPropagation(); handleLogout();}}>
+                          <LogoutOutlined style={{size: 10, marginRight: 5}} />
+                          Logout
+                        </a>),
+                    }
+                  ]
+                }}
+              >
+                <Button type="text" size="large">
+                  <Avatar
+                    style={{
+                      backgroundColor: '#aaa',
+                    }}
+                    icon={<UserOutlined />}
+                    size="small"
+                  />
+                </Button>
+              </Dropdown>
+            </Header>
+            <Content style={{margin: '0 16px', overflow: 'initial'}}>
+              {/* <Breadcrumb style={{margin: '16px 0'}}>
+                <Breadcrumb.Item>User</Breadcrumb.Item>
+                <Breadcrumb.Item>Bill</Breadcrumb.Item>
+              </Breadcrumb>
+              <div style={{padding: 24, minHeight: 360, background: colorBgContainer, borderRadius: borderRadiusLG,}}>
+                Bill is a cat.
+              </div> */}
+              <Suspense fallback={<Spin spinning={true}/>}>
+                <Routes>
+                  {routes.map((route, idx) => (
+                    route.element && (
+                      <Route
+                        key={idx}
+                        path={route.path}
+                        exact={route.exact}
+                        name={route.name}
+                        element={<route.element />}
+                      />
+                    )
+                  ))}
+                </Routes>
+              </Suspense>
+            </Content>
+            <Footer style={{ textAlign: 'center' }}>
+              Created by Virtuosoft
+            </Footer>
+          </Layout>
         </Layout>
-      </Layout>
+      </ConfigProvider>
       :
       <Navigate to='/' />
     }
